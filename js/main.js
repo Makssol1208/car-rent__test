547cd16c71d03f34991b6c735d31ad910aaca928
Maksym Soloviov <maxim.sv143@gmail.com>
2025-04-15 16:33:27 +0300

    mobile layout commit

--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@ const mobileNavButton = document.querySelector(".mobile-nav-button");
 const mobileNavIcon = document.querySelector(".mobile-nav-button__icon");
 const mobileNav = document.querySelector(".mobile-nav");
 
+
+
 mobileNavButton.addEventListener("click", function () {
   mobileNavIcon.classList.toggle("active");
   mobileNav.classList.toggle("active");
+
+  document.body.classList.toggle("body_overlay");
   document.body.classList.toggle("no-scroll");
   document.body.classList.toggle("overflow");
-  mobileNav.classList.remove("overflow");
 });
