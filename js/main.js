e1976858bac77701df8d4376fc2274474ca82850
Maksym Soloviov <maxim.sv143@gmail.com>
2025-04-15 18:14:22 +0300

    fixing js commit

--- js/main.js
+++ js/main.js
@@ -4,13 +4,6 @@ const mobileNavIcon = document.querySelector(".mobile-nav-button__icon");
 const mobileNav = document.querySelector(".mobile-nav");
 const node = document.getElementById("node");
 
-document.addEventListener("mousedown", (e) => {
-  if (!node.contains(e.target)) {
-    mobileNav.classList.remove("active");
-    document.body.classList.remove("body_overlay");
-  }
-});
-
 mobileNavButton.addEventListener("click", function () {
   mobileNavIcon.classList.toggle("active");
   mobileNav.classList.toggle("active");
@@ -18,4 +11,12 @@ mobileNavButton.addEventListener("click", function () {
   document.body.classList.toggle("body_overlay");
   document.body.classList.toggle("no-scroll");
   document.body.classList.toggle("overflow");
+});
+
+document.addEventListener("mousedown", (e) => {
+  if (!node.contains(e.target) && !mobileNavButton.contains(e.target)) {
+    mobileNav.classList.remove("active");
+    document.body.classList.remove("body_overlay");
+    document.body.classList.remove("overflow");
+  }
 });
